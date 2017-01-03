3c49ac960315504a2a098a809a30cd68887d7be9
lombardo-chcg <dpa.lombardo@gmail.com>
2017-01-02 21:56:23 -0600

    update date format

--- scripts/make-post-header.sh
+++ scripts/make-post-header.sh
@@ -4,7 +4,7 @@ function makePostHeader() {
   [[ $# -ne 1 ]] && { echo "makePostHeader: Must supply name of post as string passed to script" >&2; exit 1; }
 
   postTitle="$1"
-  postDate=$(date +"%D %T")
+  postDate=$(date +"%Y-%m-%d %T")
 
   echo -e "---
 layout: post
