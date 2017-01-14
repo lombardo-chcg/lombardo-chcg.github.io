7a866fa5d3577a15f324e71261ece8aed19107c8
lombardo-chcg <dpa.lombardo@gmail.com>
2017-01-14 09:12:56 -0600

    update mac conditional

--- build-and-serve.sh
+++ build-and-serve.sh
@@ -5,10 +5,10 @@ bundle exec jekyll serve > /dev/null &
 # ps aux | grep jekyll
 # kill -9 <jekyll_pid>
 
-sleep 1
+sleep 2
 
 # on my linux box, open jekyll site in firefox
 [[ $OSTYPE == "linux-gnu" ]] && { firefox http://localhost:4000/ 1>/dev/null 2>&1 & }
 
 # on my mac, open in chrome
-[[ $uname == "Darwin" ]] && { open -a /Applications/Google\ Chrome.app http://localhost:4000/; }
+[[ $(uname) == "Darwin" ]] && { open -a /Applications/Google\ Chrome.app http://localhost:4000/; }
