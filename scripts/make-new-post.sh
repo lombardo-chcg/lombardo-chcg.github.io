029f634cc1ae0e71b28de49bcc0dc696513917a8
lombardo-chcg <dpa.lombardo@gmail.com>
2017-01-16 19:59:06 -0600

    update script to track posts by week number

--- scripts/make-new-post.sh
+++ scripts/make-new-post.sh
@@ -5,7 +5,7 @@ source scripts/make-post-header.sh
 [[ $# -ne 1 ]] && { echo "Must supply name of post as string passed to script" >&2; exit 1; }
 
 postDate=$(date +%Y-%m-%d)
-currentMonth=$(date +%m)
+currentWeekNumber=$(date +%V)
 postTopic="$1"
 fileName=$postDate
 
@@ -14,14 +14,16 @@ for word in $postTopic; do
 done
 
 fileName=$fileName.md
-fileLocation=_posts/${currentMonth}
+fileLocation=_posts/${currentWeekNumber}
+
+[[ ! -d $fileLocation ]] && { mkdir $fileLocation; }
 
 [[ -e $fileLocation/$fileName ]] &&  { echo "Error: File already exists $fileLocation/$fileName" >&2; exit 1; }
 
 echo "Creating '$fileName' and inserting Jekyll header..."
 header=$(makePostHeader "$postTopic")
 
-echo "$header" >> _posts/${currentMonth}/${fileName}
+echo "$header" >> $fileLocation/${fileName}
 
 echo -e "\n...done."
 
