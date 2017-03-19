0ced21c414cc85925d779b27c4404c3f6701cef0
lombardo-chcg <dpa.lombardo@gmail.com>
2017-03-19 00:31:12 -0500

    update search for non-es2015 compatibility

--- js/search.js
+++ js/search.js
@@ -1,69 +1,97 @@
-window.onload = function() {
-  document.getElementById("search-box").focus();
-  let params = new URLSearchParams(document.location.search.substring(1));
-  search(params.get('term'));
-};
+(function() {
+  'use strict';
 
-function setContentByID(id, content) {
-  document.getElementById(id).innerHTML = content;
-}
-
-function clearSearchResults() {
-  [
-    'ux-feedback',
-    'top-tier-results-header',
-    'top-tier-results',
-    'lower-tier-results-header',
-    'lower-tier-results'
-  ].forEach(divId => setContentByID(divId, ''))
-}
-
-function search(term) {
-  clearSearchResults();
-  var searchRequest, searchResults;
-
-  if (!term) {
-    setContentByID('ux-feedback', '<p>must enter a search term</p>');
-    return;
+  // String.prototype.includes() Polyfill
+  if (!String.prototype.includes) {
+    String.prototype.includes = function(search) {
+      return this.indexOf(search) !== -1;
+    };
   }
 
-  searchRequest = term.toLowerCase();
+  init();
 
-  searchResults = window.postsStore.reduce((results, post) => {
-    if (post.categories.includes(searchRequest) || post.tags.includes(searchRequest) || post.excerpt.includes(searchRequest)) {
-      results.topTier.push(post);
-      return results;
-    }
-
-    if (post.content.includes(searchRequest)) {
-      results.lowerTier.push(post);
-      return results;
-    }
-
-    return results;
-  }, { topTier: [], lowerTier: [] })
-
-  if (!searchResults.topTier.length && !searchResults.lowerTier.length) {
-    setContentByID('ux-feedback', '<p>no results found</p>');
-    return;
+  function init() {
+    document.getElementById("search-box").focus();
+    // extract the query params which lie after ?term=
+    var params = document.location.search.substring(6);
+    search(params);
   }
 
-  displayResults('top-tier-results', searchResults.topTier);
-  displayResults('lower-tier-results', searchResults.lowerTier);
-}
+  function setContentByID(id, content) {
+    document.getElementById(id).innerHTML = content;
+  }
 
-function displayResults(sectionName, results) {
-  if (results.length) {
-    setContentByID(`${sectionName}-header`, sectionName.replace(/-/g, ' '));
-
-    results.forEach(result => {
-      var node = `
-        <div>
-          <a href="${result.url}">${result.title}</a>
-          <h6><i>${result.excerpt}</i></h6>
-        </div>
-      `
-      document.getElementById(sectionName).innerHTML += node;
+  function clearPageContent() {
+    [
+      'ux-feedback',
+      'top-tier-results-header',
+      'top-tier-results',
+      'lower-tier-results-header',
+      'lower-tier-results'
+    ].forEach(function(divId) {
+      return setContentByID(divId, '');
     });
   }
-}
+
+  function search(term) {
+    var searchRequest, searchResults;
+
+    clearPageContent();
+
+    if (!term) {
+      window.location.href = '/';
+      return;
+    }
+
+    // temporary workaround for multi-word searches
+    if (term.includes('%20')) {
+      term = term.split('%20')[0];
+    }
+
+    searchRequest = term.toLowerCase();
+
+    searchResults = searchPostStore(searchRequest);
+
+    if (!searchResults.topTier.length && !searchResults.lowerTier.length) {
+      setContentByID('ux-feedback', '<p>no results found</p>');
+      return;
+    }
+
+    setContentByID('ux-feedback', '<h4>"<i>' + searchRequest + '</i>"</h4>');
+    displayResults('top-tier-results', searchResults.topTier);
+    displayResults('lower-tier-results', searchResults.lowerTier);
+  }
+
+  function searchPostStore(searchRequest) {
+    // window.postsStore is set in a script tag in search.html using Jekyll's built in iteration
+    return window.postsStore.reduce(function(results, post) {
+      if (
+        post.categories.includes(searchRequest)
+        || post.tags.includes(searchRequest)
+        || post.title.includes(searchRequest)
+        || post.excerpt.includes(searchRequest)
+      ) {
+        results.topTier.push(post);
+        return results;
+      }
+
+      if (post.content.includes(searchRequest)) {
+        results.lowerTier.push(post);
+        return results;
+      }
+
+      return results;
+    }, { topTier: [], lowerTier: [] });
+  }
+
+  function displayResults(sectionName, results) {
+    if (results.length) {
+      setContentByID(sectionName + '-header', sectionName.replace(/-/g, ' ') + ' (' + results.length + ')');
+
+      results.forEach(function(result) {
+        var node = '\n        <div>\n          <a href="' + result.url + '">' + result.title + '</a>\n          <h6><i>' + result.excerpt + '</i></h6>\n        </div>\n      ';
+        document.getElementById(sectionName).innerHTML += node;
+      });
+    }
+  }
+}());
