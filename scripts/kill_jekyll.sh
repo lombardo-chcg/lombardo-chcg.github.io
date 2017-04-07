dae9e2f98e59f143aad7e60fd99135b5c70d7bb3
lombardo-chcg <dpa.lombardo@gmail.com>
2017-04-06 21:41:15 -0500

    april 6

--- scripts/kill_jekyll.sh
+++ scripts/kill_jekyll.sh
@@ -2,3 +2,4 @@
 echo "goodnight, sweet Jekyll..."
 
 ps aux | grep jekyll | grep -v grep | awk  '{print $2}' | xargs kill -9
+# alt: lsof -i TCP:4000 -sTCP:LISTEN | awk 'FNR==2 {print $2}' | xargs kill -9
